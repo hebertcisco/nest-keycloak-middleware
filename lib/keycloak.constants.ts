55d11576800b1c58259cdece15c71f32440a0dc2
hebertcisco <hebertcisco@outlook.com>
2022-06-22 22:30:46 -0300

    feat: :sparkles: add keycloak constants

---
+++ lib/keycloak.constants.ts
@@ -0,0 +1,7 @@
+export const KEYCLOACK_TOKEN = 'KEYCLOACK_TOKEN';
+export const KEYCLOACK_MODULE_ID = 'KEYCLOACK_MODULE_ID';
+export const KEYCLOACK_MODULE_OPTIONS = 'KEYCLOACK_MODULE_OPTIONS';
+
+export const KEY_START = '-----BEGIN PUBLIC KEY-----';
+export const KEY_END = '-----END PUBLIC KEY-----';
+export const DEFAULT_ALGORITHMS = ['RS256'];
